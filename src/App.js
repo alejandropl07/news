bd94588fc99ed0b56e57f4e64a3b130ac6cfee3b
alejandropl07 <aprietoleon97@gmail.com>
2022-04-18 22:54:44 -0500

    Conexion a la API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, {Component} from 'react';
 
 class App extends Component {
-  state = {}
+  state = {
+    noticias: []
+  }
+
+  componentDidMount(){
+    this.consultarApiNoticias();
+  }
+
+  consultarApiNoticias  = async ()  =>  {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=b9b441a96036442090555e3cf957784c`;
+    const respuesta = await fetch(url);
+    const noticias  = await respuesta.json();
+
+    this.setState({
+      noticias: noticias.articles
+    })
+  }
+
   render(){
     return(
       <h1>API Noticias</h1>
